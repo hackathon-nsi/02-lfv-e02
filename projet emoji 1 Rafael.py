20cd89625ac9403b0c62461049f74ca14ec488f9
nsi-lfv-chr <79856220+nsi-lfv-chr@users.noreply.github.com>
2021-04-15 09:49:07 +0200

    Delete projet emoji 1 Rafael.py

--- projet emoji 1 Rafael.py
+++
@@ -1,9 +0,0 @@
-msg=input("votre message")
-msg=str(msg)
-msg=msg.split(" ")
-dic={"princesse":"\U0001F609\n","prince":"\U0001F934\n","avion":"\U00002708\n"}
-for mot in msg:
-    if mot in dic:
-        print(dic[mot],end=" ")
-    else :
-        print(mot,end=" ")
